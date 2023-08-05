483a9178c49e5b031acc2f7bbe96655b6c9de483
Victor Kipkorir <victorquaint@gmail.com>
2023-08-05 23:20:05 +0300

    Agrisolve-admin

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,33 @@ import "./App.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Mainbar from "./Components/Mainbar/Mainbar";
 import axios from "axios";
+import { useHistory } from "react-router-use-history";
 
-function App() {
+function useAuth() {
 	const [loggedIn, setLoggedIn] = useState(false);
 	const [user, setUser] = useState(null);
 	const [token, setToken] = useState(null);
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(false);
-	const [message, setMessage] = useState(null);
-	const [success, setSuccess] = useState(false);
+	const history = useHistory();
+
+	useEffect(() => {
+		const storedUser = JSON.parse(localStorage.getItem("agrisolveData"));
+		if (!storedUser || storedUser.loginStatus !== "loggedIn") {
+			history.push(
+				"https://64ceabd3dad64a69c92babad--agrisolve.netlify.app/login"
+			);
+		} else {
+			setUser(storedUser);
+			setToken(storedUser.token);
+			setLoggedIn(true);
+		}
+	}, [history]);
+
+	return { loggedIn, user, token };
+}
+
+function App() {
+	const { loggedIn, user, token } = useAuth();
 	const [users, setUsers] = useState([]);
-	const [userData, setUserData] = useState(null);
 	const [userDataLoaded, setUserDataLoaded] = useState(false);
 	const [products, setProducts] = useState(null);
 	const [requests, setRequests] = useState(null);
@@ -36,48 +52,21 @@ function App() {
 	};
 
 	useEffect(() => {
-		const fetchUserData = async () => {
-			const storedUser = JSON.parse(localStorage.getItem("agrisolveData"));
-			if (storedUser) {
-				try {
-					const response = await axios.get(
-						`https://agrisolve-techsupport254.vercel.app/auth/user/${storedUser.email}`,
-						{
-							headers: {
-								"x-auth-token": storedUser.token,
-							},
-						}
-					);
-
-					setUser(response.data);
-
-					if (response.data.loginStatus === "loggedIn") {
-						setLoggedIn(true);
-					}
-				} catch (err) {
-					console.log(err);
-				}
-			}
-
-			setUserDataLoaded(true);
-		};
-
-		fetchUserData();
 		fetchAllUsers();
-	}, [token, userData]);
-
-	const fetchProducts = async () => {
-		try {
-			const resData = await axios.get(
-				"https://api.escuelajs.co/api/v1/products"
-			);
-			setProducts(resData.data);
-		} catch (err) {
-			console.log(err);
-		}
-	};
+	}, [token]);
 
 	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const resData = await axios.get(
+					"https://api.escuelajs.co/api/v1/products"
+				);
+				setProducts(resData.data);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+
 		fetchProducts();
 	}, []);
 
@@ -93,11 +82,9 @@ function App() {
 			hour12: false,
 		};
 
-		// Calculate the time difference in milliseconds
 		const timeDiffInMilliseconds = Date.now() - date.getTime();
 		const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
 
-		// If the time difference is more than 3 days, format the date without the time
 		if (timeDiffInMilliseconds >= 3 * oneDayInMilliseconds) {
 			return date.toLocaleString("en-US", {
 				...options,
@@ -106,31 +93,23 @@ function App() {
 				second: undefined,
 			});
 		} else if (timeDiffInMilliseconds >= 2 * oneDayInMilliseconds) {
-			// If the time difference is more than 1 day, but not more than 3 days, show "Yesterday"
 			return "Yesterday";
 		} else if (timeDiffInMilliseconds >= oneDayInMilliseconds) {
-			// If the time difference is less than 1 day, show "x days ago"
 			return `${Math.floor(
 				timeDiffInMilliseconds / oneDayInMilliseconds
 			)} days ago`;
 		} else if (timeDiffInMilliseconds >= 60 * 60 * 1000) {
-			// If the time difference is less than 1 day, show "x hours ago"
 			return `${Math.floor(
 				timeDiffInMilliseconds / (60 * 60 * 1000)
 			)} hours ago`;
 		} else if (timeDiffInMilliseconds >= 60 * 1000) {
-			// If the time difference is less than 1 hour, but more than 1 minute, show "x minutes ago"
 			return `${Math.floor(timeDiffInMilliseconds / (60 * 1000))} minutes ago`;
 		} else if (timeDiffInMilliseconds >= 1000) {
-			// If the time difference is less than 1 minute, but more than 1 second, show "x seconds ago"
 			return `${Math.floor(timeDiffInMilliseconds / 1000)} seconds ago`;
 		}
 
-		// If the time difference is less than 1 second, show the exact date and time
 		return date.toLocaleString("en-US", options);
 	};
-
-	// requests
 
 	const fetchRequest = async (url, method, data) => {
 		try {
@@ -151,7 +130,7 @@ function App() {
 	useEffect(() => {
 		if (user) {
 			fetchRequest(
-				`https://agrisolve-techsupport254.vercel.app/consults/consults`,
+				"https://agrisolve-techsupport254.vercel.app/consults/consults",
 				"GET"
 			);
 		}
@@ -160,7 +139,7 @@ function App() {
 	return (
 		<div className="App flex flex-col md:flex-row w-full h-screen">
 			<div className="Sidebar w-1/2 md:w-1/5 p-1 border-r-2 bg-slate-800 text-white">
-				<Sidebar user={user} />
+				{loggedIn && <Sidebar user={user} />}
 			</div>
 			<div className="Mainbar w-full md:w-4/5 p-1 border-r-2 bg-slate-200 text-black">
 				<Mainbar
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -3,28 +3,14 @@ import "./Sidebar.css";
 import { Link, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
 import { sidebardata } from "../../Data";
-import "react-loading-skeleton/dist/skeleton.css";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
-const Sidebar = ({ user, sidebarToggle }) => {
-	const [toggle, setToggle] = React.useState(false);
+const Sidebar = ({ user }) => {
 	const location = useLocation();
-
-	const handleSidebarItemClick = () => {
-		setToggle(!toggle);
-	};
-
-	React.useEffect(() => {
-		if (sidebarToggle) {
-			setToggle(true);
-		} else {
-			setToggle(false);
-		}
-	}, [sidebarToggle]);
 
 	return (
 		<div className="SidebarContainer">
 			<div className="SidebarTop">
+				{/* Profile section */}
 				<div className="ProfilePic flex justify-center items-center">
 					<img
 						src={
@@ -39,6 +25,8 @@ const Sidebar = ({ user, sidebarToggle }) => {
 					<small>{user?.username}</small>
 					<p>{user?.userType}</p>
 				</div>
+
+				{/* Sidebar items */}
 				<div className="SidebarItems">
 					{sidebardata.map((item) => {
 						const isActive = location.pathname === item.path;
@@ -47,11 +35,11 @@ const Sidebar = ({ user, sidebarToggle }) => {
 								key={item.id}
 								to={item.path}
 								className={`SidebarItemLink ${isActive ? "Active" : ""}`}
-								onClick={handleSidebarItemClick}
 							>
 								<motion.div
-									className={`SidebarItem ${isActive ? "Active" : ""}`}
-									animate={{ scale: isActive ? 1.05 : 1 }}
+									className="SidebarItem"
+									whileHover={{ scale: 1.05 }}
+									whileTap={{ scale: 0.95 }}
 								>
 									{item.item}
 									<div className="Icon text-teal-300">{item.icon}</div>
